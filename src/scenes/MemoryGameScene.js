1592cc1266c3cb82a7dfd39269c237864e69d317
Ahlijati Nuraminah <ahlijati.nuraminah@gmail.com>
2022-05-19 10:10:40 +0700

    Meeting 7: Open the Box

--- src/scenes/MemoryGameScene.js
+++ src/scenes/MemoryGameScene.js
@@ -1,5 +1,11 @@
 import Phaser from 'phaser'
 
+const level = [
+	[1, 0, 3],
+	[2, 4, 1],
+	[3, 4, 2]
+]
+
 export default class MemoryGameScene extends Phaser.Scene
 {
 	constructor() {
@@ -12,6 +18,8 @@ export default class MemoryGameScene extends Phaser.Scene
         this.boxGroup = undefined
         this.player = undefined
 		this.cursors = this.input.keyboard.createCursorKeys()
+        this.activeBox = undefined
+        this.itemsGroup = undefined
     }
 
 	preload() {
@@ -28,15 +36,31 @@ export default class MemoryGameScene extends Phaser.Scene
     }
 
     create() {
-        this.add.image(this.halfWidth, 0, 'bg').setScale(7)
+        this.add.image(this.halfWidth, 150, 'bg').setScale(3)
         this.boxGroup = this.physics.add.staticGroup()
         this.createBoxes()
         this.player = this.createPlayer()
-        
+
+        this.physics.add.collider(this.player, this.boxGroup, this.handlePlayerBoxCollide, undefined, this)
+        this.itemsGroup = this.add.group()
     }
 
     update() {
         this.movePlayer(this.player)
+
+        this.children.each(c => {
+			// @ts-ignore
+			const child = c
+
+			if (child.getData('sorted'))
+			{
+				return
+			}
+
+			child.setDepth(child.y)
+		})
+
+        this.updateActiveBox()
     }
 
     createBoxes()
@@ -49,6 +73,10 @@ export default class MemoryGameScene extends Phaser.Scene
 			for (let col = 0; col < 3; ++col)
 			{
 				this.boxGroup.get(width * xPer, y, 'tilesheet', 7)
+                    .setSize(64, 32)
+					.setOffset(0, 32)
+                    .setData('itemType', level[row][col])
+
 				xPer += 0.25
 			}
 
@@ -129,5 +157,102 @@ export default class MemoryGameScene extends Phaser.Scene
 			this.player.setVelocity(0, 0)
 			this.player.anims.play('standby')	
 		}
+
+        const spaceJustPressed = Phaser.Input.Keyboard.JustUp(this.cursors.space)
+		if (spaceJustPressed && this.activeBox)
+		{
+			this.openBox(this.activeBox)
+
+			this.activeBox.setFrame(7)
+			this.activeBox = undefined
+		}
+	}
+
+    handlePlayerBoxCollide(player, box)
+	{		
+		if (this.activeBox)
+		{
+			return
+		}
+
+		this.activeBox = box
+		this.activeBox.setFrame(9)
+
+        const opened = box.getData('opened')
+		
+		if (opened)
+		{
+			return
+		}
+	}
+
+    updateActiveBox()
+	{
+		if (!this.activeBox)
+		{
+			return
+		}
+
+		const distance = Phaser.Math.Distance.Between(
+			this.player.x, this.player.y,
+			this.activeBox.x, this.activeBox.y
+		)
+
+		if (distance < 64)
+		{
+			return
+		}
+
+		this.activeBox.setFrame(7)
+		this.activeBox = undefined
+	}
+
+    openBox(box)
+	{
+		if(!box)
+		{
+			return
+		}
+
+		const itemType = box.getData('itemType')
+		// console.log(itemType)
+		
+		/** @type {Phaser.GameObjects.Sprite} */
+		let item
+
+		switch (itemType)
+		{
+			case 0:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('bear')
+				break
+
+			case 1:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('chicken')
+				break
+
+			case 2:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('duck')
+				break
+
+			case 3:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('parrot')
+				break
+
+			case 4:
+				item = this.itemsGroup.get(box.x, box.y)
+				item.setTexture('penguin')
+				break
+		}
+
+		if (!item)
+		{
+			return
+		}
+
+		box.setData('opened', true)		
 	}
 }
