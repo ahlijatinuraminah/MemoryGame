7309329bdc2a8f210d536e9f3365b42b0d306635
Ahlijati Nuraminah <ahlijati.nuraminah@gmail.com>
2022-05-19 09:37:08 +0700

    initial commit

---
+++ src/scenes/MemoryGameScene.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+export default class MemoryGameScene extends Phaser.Scene
+{
+	constructor()
+	{
+		super('memory-game-scene')
+	}
+
+	preload()
+    {
+        
+    }
+
+    create()
+    {
+        
+    }
+}
